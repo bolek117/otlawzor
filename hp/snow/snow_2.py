22c2b3bfc74e6d04d2bd2cc490a7b47c745b426f
mwitas <mwitas@local>
2015-08-24 12:09:55 +0200

    Task snow solved

--- hp/snow/snow_2.py
+++ hp/snow/snow_2.py
@@ -1,1 +1,26 @@
 __author__ = 'bolek117'
+
+if __name__ == '__main__':
+    f = open('snow.txt', 'r')
+
+    as_bin = []
+    for line in f:
+        if line.find(' \n') != -1:
+            as_bin.append('1')
+        else:
+            as_bin.append('0')
+
+    as_bin_str = ''.join(as_bin)
+
+    flag = []
+    for i in xrange(len(as_bin_str)/8):
+        tmp = []
+        for j in xrange(8):
+            tmp.append(as_bin_str[i * 8 + j])
+
+        num = ''.join(tmp)
+        flag.append(chr(int(num, 2)))
+
+    print ''.join(flag)
+
+    pass
